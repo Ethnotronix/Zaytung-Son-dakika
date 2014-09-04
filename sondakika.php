ed45eb06bff17eee6ad89bc47d88c6dae9ea65cf
Ethnotronix <alican_kaya@hotmail.com.tr>
2014-09-04 23:49:52 +0300

    Jquery Ticker eklendi
    Jquery Ticker ile güzel bir görünüm elde edildi.

--- sondakika.php
+++ sondakika.php
@@ -1,3 +1,13 @@
+<html>
+<head>
+<link href="needs/ticker-style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
+<script src="needs/jquery.ticker.js" type="text/javascript"></script>
+<script type="text/javascript">$(function () {$('#js-news').ticker();});</script>
+</head>
+<body>
+<ul id="js-news" class="js-hidden">
+  
 <?php 
  
 $veri = file_get_contents("http://www.zaytung.com/index.asp");
@@ -6,6 +16,9 @@ preg_match_all('@style="color:black;"><font size="-1">(.*?)</font></a>@si',$veri
 
 for ($i=0; $i<count($sonuc[1]); $i++)
 {
-echo "",$sonuc[1][$i],"<br>\n";
+
+echo "<li class='news-item'>",$sonuc[1][$i],"</li>\n";
 }
 ?>
+</ul>
+
